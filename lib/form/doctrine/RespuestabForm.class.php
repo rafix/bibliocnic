a8e14640602613a6a97ca926a3672646e3da7394
Pablo Reyes <pablo.reyes.mail@gmail.com>
2011-05-31 13:09:26 +0200

    1er commit upgradeando schema

---
+++ lib/form/doctrine/RespuestabForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Respuestab form.
+ *
+ * @package    bibliocnic
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class RespuestabForm extends BaseRespuestabForm
+{
+  public function configure()
+  {
+  }
+}
